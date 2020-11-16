5514015a42b08dddba427ccbf7aa7928bb75265f
lkw199711 <lkw199711@163.com>
2020-11-16 16:16:44 +0800

    feat:成功加载页面

--- app/src/main/java/com/example/manga_app_java/MainActivity.java
+++ app/src/main/java/com/example/manga_app_java/MainActivity.java
@@ -14,30 +14,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webview = (WebView)findViewById(R.id.webview);
-        /*
+
         //是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
         webview.getSettings().setJavaScriptEnabled(true);
         //设置js可以直接打开窗口，如window.open()，默认为false
         webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         //是否使用缓存
         webview.getSettings().setAppCacheEnabled(true);
-        */
 
-
-        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
-        webview.getSettings().setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
-        webview.getSettings().setSupportZoom(true);//是否可以缩放，默认true
-        webview.getSettings().setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
-        webview.getSettings().setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
-        webview.getSettings().setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
-        webview.getSettings().setAppCacheEnabled(true);//是否使用缓存
         webview.getSettings().setDomStorageEnabled(true);//DOM Storage
-        // displayWebview.getSettings().setUserAgentString("User-Agent:Android");//设置用户代理，一般不用
+
+        webview.getSettings().setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+
+        webview.getSettings().setSupportZoom(true);//是否可以缩放，默认true
+
+        webview.getSettings().setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+
         //加载地址
-        webview.loadUrl("https://tt2.ink");
+        webview.loadUrl("https://manga.tt2.ink");
 
-//        webview.setWebViewClient("http://manga.tt2.ink")
-
+        //加载webView自己的环境 去加载页面
         webview.setWebViewClient(new WebViewClient() {
             @Override
 
@@ -50,5 +46,18 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
+        /*网上的配置示例
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        webview.getSettings().setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
+        webview.getSettings().setSupportZoom(true);//是否可以缩放，默认true
+        webview.getSettings().setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        webview.getSettings().setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        webview.getSettings().setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        webview.getSettings().setAppCacheEnabled(true);//是否使用缓存
+        webview.getSettings().setDomStorageEnabled(true);//DOM Storage
+        // displayWebview.getSettings().setUserAgentString("User-Agent:Android");//设置用户代理，一般不用
+        */
     }
 }
